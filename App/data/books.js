9e086c1368ba53aa19910ca90091f779430fd53b
Connor Lariviere <71781445+clarivi@users.noreply.github.com>
2024-07-19 14:34:03 -0400

    Add files via upload

---
+++ App/data/books.js
@@ -0,0 +1,38 @@
+export const books = 
+[
+    {
+        "title": "Dragon Rider",
+        "author": "Cornelia Funke",
+        "pages": "523",
+        "genre": "Fantasy",
+        "description": "With a lonely boy named Ben on board, the brave young dragon Firedrake sets out on a magical journey to find the mythical place where silver dragons can live in peace forever. Flying over moonlit lands and sparkling seas, they encounter fantastic creatures, summon up surprising courage -- and cross the path of a ruthless villain with an ancient grudge who's determined to end their quest. Only a secret destiny can save the dragons in this enchanting adventure about the true meaning of home.",
+    },
+    {
+        "title": "Murder on the Orient Express",
+        "author": "Agatha Christie",
+        "pages": "256",
+        "genre": "Murder Mystery",
+        "description": "Just after midnight, the famous Orient Express is stopped in its tracks by a snowdrift. By morning, the millionaire Samuel Edward Ratchett lies dead in his compartment, stabbed a dozen times, his door locked from the inside. Without a shred of doubt, one of his fellow passengers is the murderer. Isolated by the storm, detective Hercule Poirot must find the killer among a dozen of the dead man’s enemies, before the murderer decides to strike again.",
+    },
+    {
+        "title": "Murtagh",
+        "author": "Christopher Paolini",
+        "pages": "663",
+        "genre": "Fantasy",
+        "description": "Throughout the land, hushed voices whisper of brittle ground and a faint scent of brimstone in the air—and Murtagh senses that something wicked lurks in the shadows of Alagaësia. So begins an epic journey into lands both familiar and untraveled, where Murtagh and Thorn must use every weapon in their arsenal, from brains to brawn, to find and outwit a mysterious witch. A witch who is much more than she seems.",
+    },
+    {
+        "title": "Scythe",
+        "author": "Neal Shusterman",
+        "pages": "443",
+        "genre": "Science Ficton",
+        "description": "A world with no hunger, no disease, no war, no misery: Humanity has conquered all those things and has even conquered death. Now Scythes are the only ones who can end life - and they are commanded to do so in order to keep the size of the population under control. Citra and Rowan are chosen to apprentice to a scythe - a role that neither wants. These teens must master the 'art' of taking life, knowing that the consequence of failure could mean losing their own.",
+    },
+    {
+        "title": "The Unwanteds",
+        "author": "Lisa McMann",
+        "pages": "416",
+        "genre": "Fantasy",
+        "description": "When Alex finds out he is Unwanted, he expects to die. That is the way of the people of Quill: Each year all the 13-year-olds are labeled as Wanted, Necessary, or Unwanted. Wanteds get more schooling and train to join the army. Necessaries keep the farms running. Unwanteds are set for elimination. It’s hard for Alex to leave behind his twin, Aaron, a Wanted, but he makes peace with his fate - until he discovers that instead of a “death farm”, what awaits him is a magical place called Artime. There, Alex and his fellow Unwanteds are encouraged to cultivate their creative abilities and use them magically. Everything Alex has ever known changes before his eyes, and it’s a wondrous transformation.",
+    }
+]
